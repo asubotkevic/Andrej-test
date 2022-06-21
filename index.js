75ccaa1907b13a7f75cbaf5a89f1fe828182732b
asubotkevic <asubotkevic@yahoo.com>
2022-06-21 14:54:56 +0300

    pirmas komitas testo

---
+++ index.js
@@ -0,0 +1,39 @@
+// Kintamuju inicijavimas
+
+//    node index.js
+
+const a = 10;
+console.log(a);
+
+const b = 8;
+console.log(b);
+
+const c = 4;
+console.log(c);
+
+const at = 'at';
+console.log(at);
+
+const bt = 'bt';
+console.log(bt);
+
+const ct = 'ct';
+console.log(ct);
+
+const skaiciai1 = [1, 3, 5, 7, 9];
+console.log(skaiciai1);
+
+const skaiciai2 = [2, 4, 6, 8, 10];
+console.log(skaiciai2);
+
+const skaiciai3 = [2, 3, 0, -4, 23];
+console.log(skaiciai3);
+
+const text1 = ['sukurti', 'kintamuosius', 'su', 'teksto', 'reiksmemis'];
+console.log(text1);
+
+const text2 = ['sukurti', 'kintamuosius', 'su', 'penkiomis', 'reiksmemis']
+console.log(text2);
+
+const text3 = ['po', 'kiekvieno', 'ju', 'inicijavimo', 'isvesti'];
+console.log(text3);
